d47a00df3718fca7aa8eef5013058109447b9458
yazdan <mohammadiy207@gmail.com>
2025-05-29 19:06:05 +0430

    adding maintenance mode

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>We'll be back soon!</title>
+    <style>
+        body {
+            background: #f8fafc;
+            color: #636b6f;
+            font-family: 'Arial', sans-serif;
+            text-align: center;
+            margin-top: 20%;
+        }
+        h1 {
+            font-size: 40px;
+            margin-bottom: 20px;
+        }
+        p {
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+    <h1>🚧 Under Maintenance</h1>
+    <p>We are performing scheduled maintenance.<br>Please check back later.</p>
+</body>
+</html>
